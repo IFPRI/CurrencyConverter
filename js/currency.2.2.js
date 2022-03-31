6741db89c86cd02341fe5b64ce760f5079e2d4ce
Yunchul Jung <y.jung@cgiar.org>
2022-03-31 18:00:07 -0400

    Apr 2022 update

--- js/currency.2.2.js
+++ js/currency.2.2.js
@@ -102,7 +102,7 @@ function loadCSVList(){
 		htmlString += '<csv>currency_7.1.2021.csv</csv>';
 		htmlString += '<csv>currency_6.1.2021.csv</csv>';
 		htmlString += '<csv>currency_5.1.2021.csv</csv>';
-		htmlString += '<csv>currency_4.1.2021.csv</csv>';
+		htmlString += '<csv>currency_4.1.2022.csv</csv>';
 		htmlString += '<csv>currency_3.1.2022.csv</csv>';
 		htmlString += '<csv>currency_2.1.2022.csv</csv>';
 		htmlString += '<csv>currency_1.1.2022.csv</csv>';
